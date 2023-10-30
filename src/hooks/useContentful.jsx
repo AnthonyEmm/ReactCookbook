440199a432905fbe7cc3f547355d1f7f8b037836
Pia <pia.koester@hey.com>
2023-10-30 17:41:12 +0100

    create client for contentful using space and accessToken

---
+++ src/hooks/useContentful.jsx
@@ -0,0 +1,24 @@
+import { createClient } from "contentful";
+
+export default function useContentful() {
+ // create the client using the space and access token
+ const client = createClient(
+    { 
+        space: '8fkfh7aut4xt',
+        accessToken: 'w5hqQCzfv_RvK7Ez9fJbgXPhLA9GdACpaUYITWBJcHg',
+        host: 'preview.contentful.com'
+    }
+ )
+
+ 
+
+
+ // get the data using the contenttype 
+
+ // clean up the data 
+
+ //return the function
+
+
+
+}
