2431c56e2149e1ec4fbab53b6b9c2763fe5e4a2a
Pia <pia.koester@hey.com>
2023-10-30 18:11:41 +0100

    create contentful hook, importing first data from database, showing title, image and dedescription on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import useContentful from './hooks/useContentful'
 import './App.css'
 
-// create recipeCard component
-// create useContentful hook
+// create recipeCard component 
+// create useContentful hook ✅
 // show recipeCard with data from contentful on page
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {getData} = useContentful(); // using the function which we have created in the hook
+  const [recipes, setRecipes] = useState(null); // setting up an empty useState for our recipe array
+
+useEffect(()=>{
+  getData().then((data) =>{
+    setRecipes(data)
+  }
+  ).catch((error)=>{console.log(error)})
+}, [])
+
+console.log(recipes)
 
   return (
     <>
+{!recipes ? <p>let's cook something together</p>: recipes.map((item) => {
+  return (
+// create recipe Card component styling with bootstrap
 
+  <div key={item.id}>
+    <h2>{item.title}</h2>
+    <img src={item.image} alt={item.title} />
+    <p>{item.description}</p>
+    </div>
+  )
+}
+
+)}
     </>
   )
 }
--- src/hooks/useContentful.jsx
+++ src/hooks/useContentful.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "contentful";
 
 export default function useContentful() {
- // create the client using the space and access token
+ // create the client using the space and access token ✅
  const client = createClient(
     { 
         space: '8fkfh7aut4xt',
@@ -10,15 +10,41 @@ export default function useContentful() {
     }
  )
 
- 
-
-
  // get the data using the contenttype 
+ const getData = async () =>{
+ // try part for if it is successfull
+ try{
+    const recipes = await client.getEntries(
+        {
+            content_type: "recipe", 
+            select: "fields"
+        }
+    ); 
+    
+    const sanitizedRecipes = recipes.items.map((recipe)=>{
+        const title = recipe.fields.recipetitle
+        const description = recipe.fields.description
+        const image = recipe.fields.image.fields.file.url
+        const id = recipe.sys.id
+        // figure out what to do with the ingredients
+
+        return {title, description, image, id}
+    }
+
+    )
+
+    console.log(recipes.items)
+    return sanitizedRecipes; // having to return the content otherwise the data in apps will be undefined
+ }
+ // catch part for if it fails
+catch(error) { console.log(error)}
+ }
+
 
  // clean up the data 
 
  //return the function
 
-
+return {getData}
 
 }
