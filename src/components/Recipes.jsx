7de0d79a1e325d2dbef3d7a916b3658a01ac41b8
unknown <nna.emesowum@gmail.com>
2023-10-31 19:34:12 +0100

    created a recipe and styled it

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Recipes = () => {
   return (
-    <div>
-      <h1>Recipes</h1>
+    <div class="card">
+      <img src="./images/Nkwobi.jpg" alt="Card Image" class="card-image" />
+      <h2 class="card-title">Nkwobi</h2>
+      <p class="card-text">
+        Nkwobi is a tantalizing Nigerian delicacy that is celebrated for its
+        rich and robust flavors. Nkwobi consists of succulent pieces of cow's
+        foot or assorted meats, simmered to tender perfection, then bathed in a
+        luscious palm oil-based sauce. The sauce, infused with spices like utazi
+        leaves, ground crayfish, and scotch bonnet peppers, imparts a fiery,
+        earthy taste. Nkwobi is typically served in small wooden or ceramic
+        bowls, accompanied by chilled drinks, making it a beloved choice for
+        communal gatherings and celebrations, offering an explosion of taste and
+        tradition in every bite.
+      </p>
+      <button className="btn">Read More</button>
     </div>
   );
 };
