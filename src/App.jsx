5adcc285362058944b1ccdcfef1742061588a971
Pia <pia.koester@hey.com>
2023-10-31 20:31:36 +0100

    create Hook for single Entry retrieval, create Routing on Overview page

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,26 @@ import Header from "./components/Header";
 import { Spinner, SimpleGrid } from "@chakra-ui/react";
 import RecipeCard from "./components/RecipeCard";
 import Recipes from "./components/Recipes";
+import Overview from "./components/Overview"
+import { Routes, Route, NavLink } from "react-router-dom";
 
-// create recipeCard component
+// create recipeCard component ✅
 // create useContentful hook ✅
-// show recipeCard with data from contentful on page
+// show recipeCard with data from contentful on page ✅
+// create routing for the recipe overview and recipe detail pages 
 
 function App() {
-  const { getData } = useContentful(); // using the function which we have created in the hook
-  const [recipes, setRecipes] = useState(null); // setting up an empty useState for our recipe array
-
-  useEffect(() => {
-    getData()
-      .then((data) => {
-        setRecipes(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  console.log(recipes);
 
   return (
     <>
       <Header />
       <h2>Recipes</h2>
-      <SimpleGrid
-        spacing={4}
-        templateColumns="repeat(auto-fill, minmax(500px, 1fr))"
-      >
-        {!recipes ? (
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
-          />
-        ) : (
-          recipes.map((recipe) => {
-            return <RecipeCard key={recipe.id} recipe={recipe} />;
-          })
-        )}
-      </SimpleGrid>
-      <Recipes />
+
+          <Routes>
+            <Route path="/" element={<h1>REcipes loading</h1>} />
+            <Route path="/recipes" element={<Overview />} />
+            <Route path="/recipes/:someId" element={<Recipes />} />
+          </Routes>
     </>
   );
 }
