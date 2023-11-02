40b0ce03677ec0afafda151254e49422ed20b134
Pia <pia.koester@hey.com>
2023-11-03 00:53:13 +0100

    style: more button more visible

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,11 +1,14 @@
 import {  NavLink } from "react-router-dom";
+import {  Button  } from "@chakra-ui/react";
+
 
 
 export default function WelcomePage() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <NavLink to={"recipes"}>Explore More</NavLink>
+      <Button colorScheme='red' size={"lg"}>
+      <NavLink to={"recipes"}>Explore More</NavLink></Button>
     </div>
   );
 }
