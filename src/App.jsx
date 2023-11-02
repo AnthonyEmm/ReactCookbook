b6f64c8a038e6107e829ef37cb7a31bd61055443
unknown <nna.emesowum@gmail.com>
2023-11-02 16:31:09 +0100

    created a new component WelcomePage and rendered it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
 import "./App.css";
 import Header from "./components/Header";
 import Recipes from "./components/Recipes";
 import Overview from "./components/Overview";
 import Form from "./components/Form";
 import { Routes, Route, NavLink } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
 
 // create recipeCard component ✅
 // create useContentful hook ✅
@@ -15,10 +15,12 @@ function App() {
   return (
     <>
       <Header />
+
       <Routes>
-        <Route path="/" element={<Overview />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/recipes" element={<Overview />} />
         <Route path="/recipes/:someId" element={<Recipes />} />
-        <Route path="/create" element={<Form/>}/>
+        <Route path="/create" element={<Form />} />
       </Routes>
     </>
   );
---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function WelcomePage() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button>Explore More</button>
+    </div>
+  );
+}
